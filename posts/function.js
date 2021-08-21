4d542864df888dfd37344b0289b42b9823fb22c7
차한음 <ihaneum@chahan-eum-ui-MacBookAir.local>
2021-08-21 18:01:57 +0900

    commit: 135 Remove progressbar function

--- posts/function.js
+++
@@ -1,3 +0,0 @@
-window.onscroll = function() {
-     document.getElementById("progressbar").style.width = ((document.body.scrollTop || document.documentElement.scrollTop / (document.documentElement.scrollHeight - document.documentElement.clientHeight)) * 100) + "%";
-};
